95a2bbd2e090d95ea89e4aa41387a35d29cfe033
Sobuz391 <monowerhossaisobuz@gmail.com>
2024-05-15 14:27:22 +0600

    customized banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div className="hero container mx-auto h-96 rounded-xl" style={{backgroundImage: 'url(https://www.shutterstock.com/image-photo/bloggingblog-concepts-ideas-white-worktable-600nw-1029506242.jpg)'}}>
-  <div className="hero-overlay rounded-xl bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+        <div className="banner container mx-auto rounded-xl  bg-gradient-to-r from-gray-900 to-cyan-900 text-white flex items-center justify-center">
+  <div className=" mx-auto px-8 py-16">
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+      <div>
+        <h1 className="text-3xl lg:text-5xl font-bold leading-tight">Explore the Latest Blog Posts</h1>
+        <p className="mt-4 text-lg">Discover insightful articles, tips, and stories written by passionate writers.</p>
+        <Link to='/allblogs' className="mt-6 inline-block px-6 py-3 rounded-lg bg-white text-blue-700 font-bold hover:bg-blue-700 hover:text-white transition duration-300">Browse Blogs</Link>
+      </div>
+      <img src="https://www.ascian.in/creatives/wp-content/uploads/blog.jpg" alt="Banner Image" className="w-full rounded-lg h-full  shadow-lg" />
     </div>
   </div>
 </div>
+
     );
 };
 
